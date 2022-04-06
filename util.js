42b70246222593448df60dc53810a2f4f59be4d9
Nikhil Kumar (IN81114) <m1081114@mindtree.com>
2022-04-06 18:09:53 +0530

    updated util.js

--- util.js
+++ util.js
@@ -1,46 +1,24 @@
-const modifyResponse = (arr) => { 
+const modifyKeys = (keysMap, obj) => {
 
+  renameKeys = (keysMap, obj) =>
+    Object.keys(obj).reduce(
+      (acc, key) => ({
+        ...acc,
+        ...{ [keysMap[key] || key]: obj[key] },
+      }),
+      {}
+    );
 
-const keysMap = {
-  assignment_id: "assignmentId",
-  assignment_identifier: "assignmentIdentifier",
-  assignment_status: "assignmentStatus",
-  status_code: "statusCode",
-  field_staff_assigned_fk: "fieldStaffAssigned",
-  assignment_date: "assignmentDate",
-  assignment_time_slot: "assignmentTimeSlot",
-  assignment_time: "assignmentTime",
-  name: "name",
-  phone: "phone",
-  property_address: "propertyAddress",
-  city: "city",
-  state: "state",
-  code: "code",
-  property_location: "propertyLocation",
-  property_ownership: "propertyOwnership",
-  contact_address_location: "contactAddressLocation",
-  property_type: "propertyType",
-  rating_provided: "ratingProvided",
-};
-
-
-renameKeys = (keysMap, obj) =>
-  Object.keys(obj).reduce(
-    (acc, key) => ({
-      ...acc,
-      ...{ [keysMap[key] || key]: obj[key] },
-    }),
-    {}
-  );
-
-return arr.map((element) => {
-    element.assignment_time = new Date(`${element.assignment_time}`).getTime();
-  return renameKeys(keysMap, element);
-});
+  return renameKeys(keysMap, obj);
 
 }
 
 
-module.exports.modifyResponse = modifyResponse;
+const AssignStatusUpdateSuccessRes = {
+  status: 200,
+  message: "Assignment status has been udpated successfully",
+}
 
-// { firstName: 'Bobo', passion: 'Front-End Master' }
+module.exports.modifyKeys = modifyKeys;
+module.exports.AssignStatusUpdateSuccessRes = AssignStatusUpdateSuccessRes;
+
